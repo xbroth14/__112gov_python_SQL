29b6af68624813c49e3031078046309eac089202
葉威廷 <161096413+xbroth14@users.noreply.github.com>
2024-03-02 14:46:21 +0800

    Update lesson4_1.sql

--- lesson4_1.sql
+++ lesson4_1.sql
@@ -9,11 +9,11 @@ CREATE TABLE students
 	score INT
 );
 
-INSERT INTO students VALUES(1,'小廣','國文');
-INSERT INTO students VALUES(2,'小美','語文');
-INSERT INTO students VALUES(3,'小花','數學');
-INSERT INTO students VALUES(4,'小黑','公民');
-INSERT INTO students VALUES(5,'小白','社會');
+INSERT INTO students VALUES(1,'小廣','國文','90');
+INSERT INTO students VALUES(2,'小美','語文','80');
+INSERT INTO students VALUES(3,'小花','數學','70');
+INSERT INTO students VALUES(4,'小黑','公民','50');
+INSERT INTO students VALUES(5,'小白','社會','100');
 SELECT *
 FROM students;
 
