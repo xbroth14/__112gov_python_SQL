333392717ddf68e3599525d67c98c08df81d11f3
葉威廷 <161096413+xbroth14@users.noreply.github.com>
2024-03-02 14:47:02 +0800

    Update lesson4_1.sql

--- lesson4_1.sql
+++ lesson4_1.sql
@@ -14,6 +14,8 @@ INSERT INTO students VALUES(2,'小美','語文','80');
 INSERT INTO students VALUES(3,'小花','數學','70');
 INSERT INTO students VALUES(4,'小黑','公民','50');
 INSERT INTO students VALUES(5,'小白','社會','100');
+
+
 SELECT *
 FROM students;
 
